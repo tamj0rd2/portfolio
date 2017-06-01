51838fc62e8429516d4df949d102250129da6b89
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-06-01 15:08:32 +0100

    Save form values in state

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react'
 import { FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap'
 
 class Contact extends Component {
-  handleNameChange = () => {};
-  handleEmailChange = () => {};
-  handleMessageChange = () => {};
+  state = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  handleNameChange = e => {
+    this.setState({ name: e.target.value })
+  };
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value })
+  };
+
+  handleMessageChange = e => {
+    this.setState({ message: e.target.value })
+  };
+
   render() {
     return (
       <div>
@@ -17,7 +31,7 @@ class Contact extends Component {
               <FormControl
                 componentClass="input"
                 type="text"
-                value=""
+                value={this.state.name}
                 onChange={this.handleNameChange}
               />
             </FormGroup>
@@ -27,7 +41,7 @@ class Contact extends Component {
               <FormControl
                 componentClass="input"
                 type="email"
-                value=""
+                value={this.state.email}
                 onChange={this.handleEmailChange}
               />
             </FormGroup>
@@ -36,7 +50,7 @@ class Contact extends Component {
               <ControlLabel>Your message *</ControlLabel>
               <FormControl
                 componentClass="textarea"
-                value=""
+                value={this.state.message}
                 onChange={this.handleMessageChange}
               />
             </FormGroup>
