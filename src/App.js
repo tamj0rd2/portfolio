20cc3fd8cbc7e66095aafb75470c4d4ff1ae4dae
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-06-01 11:43:44 +0100

    Add an about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import About from './components/About.jsx'
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello, World!
+        <About />
       </div>
     )
   }
