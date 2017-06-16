332430ff2c6a6bfad69559c5bdf419fb9dacad37
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-06-16 14:06:22 +0100

    App: add output tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,3 +1,6 @@
+import { expectOne } from '../testHelpers'
+import { shallow } from 'enzyme'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
@@ -5,4 +8,16 @@ import App from '../App'
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
+})
+
+describe('output', () => {
+  let wrapper = shallow(<App />)
+
+  it('renders an About section', () => {
+    expectOne('About', wrapper)
+  })
+
+  it('renders an About section', () => {
+    expectOne('Contact', wrapper)
+  })
 })
